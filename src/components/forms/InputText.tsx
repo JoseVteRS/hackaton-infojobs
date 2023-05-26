9c632af8417daa8e09dee459230d213b40d12dbb
Jose <jvrs.90@gmail.com>
2023-05-26 19:45:44 +0200

    fix: error displayName

--- src/components/forms/InputText.tsx
+++ src/components/forms/InputText.tsx
@@ -7,22 +7,25 @@ type InputProps = React.DetailedHTMLProps<
   label: string;
 };
 
-
-export const InputText = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
-
-  return (
-    <div className="relative w-full mb-4">
-      <span className="flex text-sm mb-1 relative">
-        <label>{props.label}</label>
-      </span>
-      <div>
-        <input
-          type="text"
-          className="text-sm border-2 border-primary-l4 transition duration-300 rounded-md focus:shadow-ij-focus focus:outline-none hover:border-primary w-full h-12 p-5"
-          ref={ref}
-          {...props}
-        />
+export const InputText = forwardRef<HTMLInputElement, InputProps>(
+  (props, ref) => {
+    return (
+      <div className="relative w-full mb-4">
+        <span className="flex text-sm mb-1 relative">
+          <label>{props.label}</label>
+        </span>
+        <div>
+          <input
+            type="text"
+            className="text-sm border-2 border-primary-l4 transition duration-300 rounded-md focus:shadow-ij-focus focus:outline-none hover:border-primary w-full h-12 p-5"
+            ref={ref}
+            {...props}
+          />
+        </div>
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
+
+
+InputText.displayName = "InputText";
